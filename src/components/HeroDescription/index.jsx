997ec36d81f2364b50ad0fe4728274ebd79fe004
Orlando Flores <orlando_flores@id.uff.br>
2021-08-27 11:18:09 -0300

    feat: finishes hero description component and style

--- src/components/HeroDescription/index.jsx
+++ src/components/HeroDescription/index.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import PlayButton from '../../images/PlayButton.png';
+import heroImage from '../../images/heroImage.png';
+import './style.css'
 
 function HeroDescription() {
   return (
-    <section>
-      descrição
+    <section className="section-hero-description">
+      <div>
+        <h1 className="hero-title">Easily create or join a local nanny share with Hapu</h1>
+        <p className="hero-description">Hapu is Airbnb for nanny share. Share your home, nanny and costs and create new flexible, affordable solutions in childcare.</p>
+      </div>
+      <div className="hero-video-link">
+        <a href="/">
+          <img
+            id="play-btn"
+            src={ PlayButton }
+            alt="play button"
+          />
+        </a>
+        <a className="video-link" href="/">
+          See hapu in action (27 seconds)
+        </a>
+      </div>
+      <div className="hero-image">
+        <img src={ heroImage } alt="" />
+      </div>
     </section>
   );
 };
