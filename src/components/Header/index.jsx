46c4f0a06adc2ae97391446c1f7a68b619af0e0c
Orlando Flores <orlando_flores@id.uff.br>
2021-08-27 10:40:35 -0300

    feat: creates component Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Badge from '../../images/Badge.png';
+import './style.css';
+
+function Header() {
+  return (
+    <header>
+        <nav className="left-container">
+          <a href="/" id="badger">
+            <img src={ Badge } alt="Hapu's badge" />
+          </a>
+          <a href="/" className="header-links hero-text">
+            Create Your Nanny Share
+          </a>
+          <a href="/" className="header-links hero-text">
+            Browse Shares
+          </a> 
+          <a href="/" className="header-links hero-text">
+            Our Story
+          </a>
+        </nav>
+        <div className="right-container">
+          <button
+            type='button'
+            className="action-btn btn hero-text">
+            Become a Nanny Share Host
+          </button>
+          <button
+            type='button'
+            className="sign-btn btn hero-text">
+            Sign In
+          </button>
+        </div>
+      </header>
+  );
+}
+
+export default Header;
--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,38 +1,11 @@
 import React from 'react';
-import Badge from '../../images/Badge.png';
 import './style.css';
+import Header from '../Header';
 
 function Hero() {
   return(
     <section className="hero">
-      <header>
-        <nav className="left-container">
-          <a href="/" id="badger">
-            <img src={ Badge } alt="Hapu's badge" />
-          </a>
-          <a href="/" className="header-links header-text">
-            Create Your Nanny Share
-          </a>
-          <a href="/" className="header-links header-text">
-            Browse Shares
-          </a> 
-          <a href="/" className="header-links header-text">
-            Our Story
-          </a>
-        </nav>
-        <div className="right-container">
-          <button
-            type='button'
-            className="action-btn btn header-text">
-            Become a Nanny Share Host
-          </button>
-          <button
-            type='button'
-            className="sign-btn btn header-text">
-            Sign In
-          </button>
-        </div>
-      </header>
+      <Header />
     </section>
   );
 };
