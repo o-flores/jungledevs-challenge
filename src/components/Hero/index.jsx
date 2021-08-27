66e739f356b5f3e512b2a56c3f3b320c59fc8c20
Orlando Flores <orlando_flores@id.uff.br>
2021-08-27 10:46:51 -0300

    feat: creates component HeroDescription

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './style.css';
 import Header from '../Header';
+import HeroDescription from '../HeroDescription';
 
 function Hero() {
   return(
     <section className="hero">
       <Header />
+      <HeroDescription />
     </section>
   );
 };
---
+++ src/components/HeroDescription/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HeroDescription() {
+  return (
+    <section>
+      descrição
+    </section>
+  );
+};
+
+export default HeroDescription;
