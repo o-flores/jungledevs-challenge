b392e6efe9a763aa02e4086cf645f7ebc6f1cbdd
Orlando Flores <orlando_flores@id.uff.br>
2021-08-28 14:26:38 -0300

    feat: finishes first section of main component

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -5,25 +5,30 @@ import ImageSection1 from '../../images/ImageSection1.png';
 function Main() {
   return (
     <main>
-      <section>
+      <section className="main-section">
         <div>
-          <h2 className="section-title">Share your home, nanny and costs</h2>
+          <h2
+            className="section-title"
+          >
+            Share your home,
+            <br />
+            nanny and costs
+          </h2>
           <p className="section-paragraph-light justify">
             You have a fantastic home, a fantastic nanny and wouldn’t cutting your costs in half be
             , well, fantastic?! If only it was easy to connect with other parents
-            to share your costs? Well now it is, with Hapu.
-            <span> Hapu means tribe </span>
-            and it’s our foundational 3 tribal principles that empowers you to create and manage
-            your own tribe. A tribe that together has the power to create new affordable
+            to share your costs? Well now it is, with Hapu.&nbsp;
+            <span className="link-medium">Hapu means tribe</span>
+            &nbsp;and it’s our foundational 3 tribal principles that empowers you to create
+            and manage your own tribe. A tribe that together has the power to create new affordable
             solutions in childcare that work for you and your community.
           </p>
-          <a href="/">
+          <a href="/" className="link-medium">
             Ready to get started?
           </a>
         </div>
-        <div>
-          <img src={ImageSection1} alt="Example of a Nanny Share profile" />
-        </div>
+        <img src={ImageSection1} alt="Example of a Nanny Share profile" />
+
       </section>
     </main>
   );
