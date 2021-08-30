c87e1763b3d7ff7816d42800df046eeba08a074c
Orlando Flores <orlando_flores@id.uff.br>
2021-08-29 21:46:38 -0300

    feat: creates controlled components and add css to input

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 function Form() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
   function handleSubmit(e) {
     e.preventDefault();
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <input id="input-name" type="text" placeholder="Your Name" />
-      <input type="text" placeholder="Your Email" />
+      <input
+        value={name}
+        id="input-name"
+        type="text"
+        placeholder="Your Name"
+        onChange={({ target }) => setName(target.value)}
+      />
+      <input
+        value={email}
+        type="text"
+        placeholder="Your Email"
+        onChange={({ target }) => setEmail(target.value)}
+      />
       <button
         type="button"
         className="action-btn btn"
